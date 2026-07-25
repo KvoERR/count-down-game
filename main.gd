abb749cc3605597d587e161c2feaac608880ac6a
Leosji <super.stup2015@yandex.ru>
2026-07-25 10:43:48 +0500

    Магазин v0.001

--- main.gd
+++ main.gd
@@ -2,11 +2,25 @@ extends Control
 
 var clicks = 0
 
+var click_power = 1
+
+var auto_clicks = 0
+
 @onready var counter = $CounterLabel
 
 func _ready():
 	counter.text = "Клики: 0"
 
 func _on_click_button_pressed():
-	clicks += 1
+	clicks += click_power
 	counter.text = "Клики: " + str(clicks)
+
+
+func _on_auto_click_timer_timeout() -> void:
+	pass # Replace with function body.
+func _on_auto_click_timer_timeout():
+	clicks += auto_clicks
+
+
+func _on_shop_button_pressed() -> void:
+	pass # Replace with function body.
