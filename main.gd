93e36195826e7cb8032e1e85a2d685a6f5630eeb
KvoERR <kvkazak3@gmail.com>
2026-07-25 11:03:23 +0500

    Обратный отсчёт

--- main.gd
+++ main.gd
@@ -1,26 +1,34 @@
 extends Control
 
-var clicks = 0
-
+var clicks = 85000
 var click_power = 1
-
-var auto_clicks = 0
+var addition_time = 1  # Сколько прибавляется каждую секунду
+var auto_clicks = 0    # Не используется пока
 
 @onready var counter = $CounterLabel
+@onready var timer = $Timer
 
 func _ready():
-	counter.text = "Клики: 0"
+	timer.wait_time = 10.0
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+	
+	update_counter()
 
 func _on_click_button_pressed():
-	clicks += click_power
-	counter.text = "Клики: " + str(clicks)
+	clicks -= click_power
+	update_counter()
 
+func _on_timer_timeout():
+	clicks += timer.wait_time
+	
+	if clicks < 0:
+		clicks = 0
+	
+	update_counter()
 
-func _on_auto_click_timer_timeout() -> void:
-	pass # Replace with function body.
-func _on_auto_click_timer_timeout():
-	clicks += auto_clicks
-
-
-func _on_shop_button_pressed() -> void:
-	pass # Replace with function body.
+func update_counter():
+	var seconds = clicks / 1000
+	var milliseconds = clicks % 1000
+	counter.text = str(seconds) + "." + str(milliseconds).pad_zeros(3) + "s"
+	
